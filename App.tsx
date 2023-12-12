59ce6a6dc1032d01f9f4999429e6fb2d9ffb23ee
chandan-fr <georgesteve31019@gmail.com>
2023-12-12 18:07:19 +0530

    foreground notification handled

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Alert } from 'react-native'
+import React, { useEffect } from 'react'
+import { notificationServices, requestUserPermission } from './src/config/pushNotificationHelper'
+import messaging from '@react-native-firebase/messaging';
+
+
 
 const App = () => {
+  useEffect(() => {
+    requestUserPermission();
+    notificationServices()
+  }, [])
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async ({notification}) => {
+      // console.log(typeof notification);
+      const titlestr = JSON.stringify(notification?.title);
+      Alert.alert(titlestr, notification?.body);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text style={{fontSize: 24, color: "#000"}}>App</Text>
     </View>
   )
 }
---
+++ src/config/pushNotificationHelper.js
@@ -0,0 +1,57 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
+
+export const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+        console.log('Authorization status:', authStatus);
+        GetFCMToken();
+    }
+};
+
+const GetFCMToken = async () => {
+    let fcmToken = await AsyncStorage.getItem("@fcmtoken");
+    // console.log("old token ==>", fcmToken);
+    if (!fcmToken) {
+        try {
+            const fcmToken = await messaging().getToken();
+            if (fcmToken) {
+                // console.log("new token ==>", fcmToken);
+                await AsyncStorage.setItem("@fcmtoken", fcmToken);
+            }
+        } catch (error) {
+            console.log("from catch ==>", error);
+        }
+    }
+};
+
+export const notificationServices = () => {
+    messaging().onNotificationOpenedApp(remoteMessage => {
+        console.log(
+            'Notification caused app to open from background state:',
+            remoteMessage.notification,
+        );
+    });
+
+    // Foreground notification handling
+    messaging().onMessage(async remoteMessage => {
+        console.log("Notification caused app to show notification from foreground state:", remoteMessage);
+    });
+
+    // Check whether an initial notification is available
+    messaging()
+        .getInitialNotification()
+        .then(remoteMessage => {
+            if (remoteMessage) {
+                console.log(
+                    'Notification caused app to open from quit state:',
+                    remoteMessage.notification,
+                );
+            }
+        });
+};
