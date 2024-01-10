60e6e2999667d26f81ef7887e6d235b8779d737f
chandan-fr <georgesteve31019@gmail.com>
2024-01-10 19:09:24 +0530

    sweaper effect

--- src/screens/stacknav/ListManipulation.tsx
+++ src/screens/stacknav/ListManipulation.tsx
@@ -1,8 +1,9 @@
 import { StyleSheet, Text, View, SafeAreaView, FlatList, Image, TouchableOpacity, ActivityIndicator } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import CheckBox from '@react-native-community/checkbox';
 import LinearGradient from 'react-native-linear-gradient';
 import { createShimmerPlaceholder } from 'react-native-shimmer-placeholder'
+import { GestureHandlerRootView, Swipeable } from 'react-native-gesture-handler';
 
 const ShimmerView = createShimmerPlaceholder(LinearGradient);
 
@@ -52,7 +53,7 @@ const CustomShimmerComponent = ({ len }: CustomShimmerComponentProps): JSX.Eleme
             data={shimmerArray}
             showsVerticalScrollIndicator={false}
             keyExtractor={(_, i) => i.toString()}
-            renderItem={({item, index}) => (
+            renderItem={({ item, index }) => (
                 <>
                     <BaseShimmerComp />
                     {index == shimmerArray.length - 1 ? <View style={{ marginBottom: 15 }} /> : null}
@@ -70,10 +71,11 @@ const ListManipulation = ({ navigation }: ListManipulationProps): JSX.Element =>
     const [lazyLoad, setLazyLoad] = useState<boolean>(false);
     const [isFrstTm, setIsFrstTm] = useState<boolean>(true);
     const [toggleCheckBox, setToggleCheckBox] = useState<boolean>(false);
+    const ref = useRef<any>(null);
 
     const getProducts = () => {
         setLoading(true);
-        fetch('https://dummyjson.com/products')
+        fetch("https://dummyjson.com/products")
             .then(res => res.json())
             .then(json => {
                 const newData: Product[] = [...json.products];
@@ -146,6 +148,18 @@ const ListManipulation = ({ navigation }: ListManipulationProps): JSX.Element =>
         setProducts(newData);
     };
 
+    const leftSwipe = (): JSX.Element => (
+        <TouchableOpacity style={styles.lSwipeWrap} onPress={() => ref.current.close()}>
+            <Image style={styles.swipeIcon} source={require("../../assets/icons/pen.png")} />
+        </TouchableOpacity>
+    )
+
+    const rightSwipe = (): JSX.Element => (
+        <TouchableOpacity style={styles.rSwipeWrap} onPress={() => ref.current.close()}>
+            <Image style={styles.swipeIcon} source={require("../../assets/icons/bin.png")} />
+        </TouchableOpacity>
+    )
+
     // console.log("products==>", products);
 
     useEffect(() => {
@@ -171,46 +185,51 @@ const ListManipulation = ({ navigation }: ListManipulationProps): JSX.Element =>
                 loading ?
                     // <ActivityIndicator animating={loading} size={'large'} color={"#3CCBA1"} />
                     <CustomShimmerComponent len={30} />
-
                     :
                     <View style={{ flex: 1 }}>
                         <FlatList
                             data={products}
                             onEndReached={loadMore}
                             // ListFooterComponent={products.length < 100 ? <ActivityIndicator animating={lazyLoad} size={'large'} color={"#3CCBA1"} style={{ marginBottom: 20, marginTop: 10 }} /> : null}
-                            ListFooterComponent={products.length < 100 ? <View style={{marginBottom: 13}}><BaseShimmerComp /></View> : null}
+                            ListFooterComponent={products.length < 100 ? <View style={{ marginBottom: 13 }}><BaseShimmerComp /></View> : null}
                             showsVerticalScrollIndicator={false}
                             keyExtractor={(item, index) => index.toString()}
                             renderItem={({ item, index }) => (
-                                <>
-                                    <TouchableOpacity
-                                        style={styles.itemCont}
-                                        onLongPress={() => handleLongPress(index)}
+                                <GestureHandlerRootView >
+                                    <Swipeable
+                                        ref={ref}
+                                        renderLeftActions={leftSwipe}
+                                        renderRightActions={rightSwipe}
                                     >
-                                        <View style={styles.leftBox}>
-                                            <Image
-                                                style={{ width: 100, height: 100, borderRadius: 10, }}
-                                                source={{ uri: item?.thumbnail }}
-                                            />
-                                        </View>
+                                        <TouchableOpacity
+                                            style={styles.itemCont}
+                                            onLongPress={() => handleLongPress(index)}
+                                        >
+                                            <View style={styles.leftBox}>
+                                                <Image
+                                                    style={{ width: 100, height: 100, borderRadius: 10, }}
+                                                    source={{ uri: item?.thumbnail }}
+                                                />
+                                            </View>
 
-                                        <View style={styles.rightBox}>
-                                            <Text style={{ fontSize: 16, color: "#F19733", marginBottom: 7 }}>
-                                                {item?.title}
-                                            </Text>
-                                            <Text style={{ fontSize: 20, color: "#2EBB92" }}>₹{item?.price}</Text>
-                                            <Text style={{ fontSize: 14, color: "#808886" }}>{item?.description}</Text>
-                                        </View>
+                                            <View style={styles.rightBox}>
+                                                <Text style={{ fontSize: 16, color: "#F19733", marginBottom: 7 }}>
+                                                    {item?.title}
+                                                </Text>
+                                                <Text style={{ fontSize: 20, color: "#2EBB92" }}>₹{item?.price}</Text>
+                                                <Text style={{ fontSize: 14, color: "#808886" }}>{item?.description}</Text>
+                                            </View>
 
-                                        {toggleCheckBox && <CheckBox
-                                            disabled={false}
-                                            value={item?.isSelected}
-                                            onValueChange={(newValue) => handleCheckBox(index)}
-                                        />}
-                                    </TouchableOpacity>
+                                            {toggleCheckBox && <CheckBox
+                                                disabled={false}
+                                                value={item?.isSelected}
+                                                onValueChange={(newValue) => handleCheckBox(index)}
+                                            />}
+                                        </TouchableOpacity>
+                                    </Swipeable>
 
-                                    {index == products.length - 1 ? <View style={{ marginBottom: 10 }} /> : null}
-                                </>
+                                    {index === products.length - 1 ? <View style={{ marginBottom: 10 }} /> : null}
+                                </GestureHandlerRootView>
                             )}
                         />
                     </View>
@@ -230,11 +249,11 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         borderRadius: 15,
         elevation: 5,
-        marginTop: 10,
         marginHorizontal: 10,
         padding: 8,
         flexDirection: "row",
         alignItems: "center",
+        marginTop: 10,
     },
     leftBox: {
         borderRadius: 10,
@@ -258,5 +277,27 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         paddingHorizontal: 20,
         paddingVertical: 6,
-    }
+    },
+    lSwipeWrap: {
+        backgroundColor: "#62C1E5",
+        alignItems: "center",
+        justifyContent: "center",
+        width: 100,
+        marginLeft: 10,
+        borderRadius: 12,
+        marginTop: 10,
+    },
+    rSwipeWrap: {
+        backgroundColor: "#62C1E5",
+        alignItems: "center",
+        justifyContent: "center",
+        width: 100,
+        marginRight: 10,
+        borderRadius: 12,
+        marginTop: 10,
+    },
+    swipeIcon: {
+        width: 50,
+        height: 50,
+    },
 });
